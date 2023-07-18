cccfa56cf5bc1897bdb01b2b192ce0f8bab2c14a
adityachakra16 <adityachakra16@gmail.com>
2023-07-19 00:28:34 +0530

    feat: fetch or refresh scores based on time

--- lib/gitcoin_passport_module/passport.rb
+++ lib/gitcoin_passport_module/passport.rb
@@ -6,7 +6,6 @@ require 'json'
 class DiscourseGitcoinPassport::Passport
 
   def self.score(user_address, scorer_id)
-    puts 'ethaddress: ' + user_address.to_s
     url = URI("https://api.scorer.gitcoin.co/registry/submit-passport")
 
     https = Net::HTTP.new(url.host, url.port)
@@ -50,22 +49,27 @@ class DiscourseGitcoinPassport::Passport
     minimum_required_score
   end
 
-  def self.has_minimimum_required_score?(ethaddress, user_id, category_id, action_type)
-    minimum_required_score = DiscourseGitcoinPassport::Passport.minimum_required_score(user_id, category_id, action_type)
-    if (!ethaddress and minimum_required_score > 0)
-      return false
-    end
-    if (minimum_required_score == 0) # No minimum score required
+  def self.has_minimimum_required_score?(user, category, action_type)
+    minimum_required_score = DiscourseGitcoinPassport::Passport.minimum_required_score(user[:id], category[:id], action_type)
+    if (minimum_required_score == 0)
       return true
     end
-    score = DiscourseGitcoinPassport::Passport.score(ethaddress, SiteSetting.gitcoin_passport_scorer_id)
+
+    siwe_account = user.associated_accounts.find { |account| account[:name] == "siwe" }
+    ethaddress = siwe_account[:description] if siwe_account
+    if (!ethaddress)
+      return false
+    end
+
+    score = DiscourseGitcoinPassport::Passport.fetch_score(user)
     score.to_f >= minimum_required_score
   end
 
-  def self.available_badges_to_claim(badges, passport_score)
+  def self.available_badges_to_claim(badges, passport_score, user)
     available_badges = []
     badges.each do |badge|
-      if UserBadge.where(badge_id: bronze_badge.id).count
+      has_badge = UserBadge.where(badge_id: badge[:id], user_id: user[:id]).count > 0
+      if !has_badge
         if badge.badge_type.name == "Gold" and  passport_score.to_f >= SiteSetting.gitcoin_passport_required_to_get_unique_humanity_gold_badge.to_f
             available_badges.push(badge)
         elsif badge.badge_type.name == "Silver" and passport_score.to_f >= SiteSetting.gitcoin_passport_required_to_get_unique_humanity_silver_badge.to_f
@@ -78,34 +82,44 @@ class DiscourseGitcoinPassport::Passport
     available_badges
   end
 
-  def self.grant_badges(badges)
-    available_badges.each do |badge|
-      puts "Granting badge: #{badge.name}"
-      BadgeGranter.grant(badge, current_user)
+  def self.grant_badges(badges, user)
+    badges.each do |badge|
+      BadgeGranter.grant(badge, user)
     end
   end
 
-  def self.refresh_passport_score()
-    puts current_user.inspect
-    siwe_account = current_user.associated_accounts.find { |account| account[:name] == "siwe" }
-      ethaddress = siwe_account[:description]
+  def self.refresh_passport_score(user)
+    siwe_account = user.associated_accounts.find { |account| account[:name] == "siwe" }
+    ethaddress = siwe_account[:description] if siwe_account
+    raise Discourse::InvalidAccess.new("You must connect your wallet to refresh your score") if not ethaddress
 
-      raise Discourse::InvalidAccess.new("You must connect your wallet to view your score") if not ethaddress
+    # save the latest passport score in the user table, this is mainly done for performance reasons.
+    # We don't want to have to query the passport api every time we want to check if a person can create a topic or post
+    score = DiscourseGitcoinPassport::Passport.score(ethaddress, SiteSetting.gitcoin_passport_scorer_id)
+    user_ = User.where(id: user.id).first
+    user_.passport_score = score
+    user_.passport_score_last_update = DateTime.now
+    user_.save
 
-      # save the latest passport score in the user table, this is mainly done for performance reasons.
-      # We don't want to have to query the passport api every time we want to check if a person can create a topic or post
-      score = DiscourseGitcoinPassport::Passport.score(ethaddress, SiteSetting.gitcoin_passport_scorer_id)
-      user = User.where(id: current_user.id).first
-      user.passport_score = score
-      user.passport_score_last_update = DateTime.now
-      user.save
+    # check if the user has earned any badges and grant them
+    badges = Badge.joins(:badge_type).joins(:badge_grouping).where(badge_groupings: {
+      name: "Unique Humanity"
+    })
+    available_badges = DiscourseGitcoinPassport::Passport.available_badges_to_claim(badges, score, user_)
+    DiscourseGitcoinPassport::Passport.grant_badges(available_badges, user_)
+    return score
+  end
 
-      # check if the user has earned any badges and grant them
-      badges = Badge.joins(:badge_type).joins(:badge_grouping).where(badge_groupings: {
-        name: "Unique Humanity"
-      })
-      available_badges = DiscourseGitcoinPassport::Passport.available_badges_to_claim(badges, score)
-      DiscourseGitcoinPassport::Passport.grant_badges(available_badges)
-      return score
+  def self.fetch_score(user, force_refresh=false)
+    local_score = user.passport_score
+    local_score_last_update = user.passport_score_last_update
+    returned_score = local_score
+
+    if force_refresh
+      returned_score = DiscourseGitcoinPassport::Passport.refresh_passport_score(user)
+    elsif local_score_last_update.nil? or local_score.nil? or ((Time.at(local_score_last_update) - Time.now) > 24.hours)
+      returned_score = DiscourseGitcoinPassport::Passport.refresh_passport_score(user)
+    end
+    returned_score
   end
 end
